64bbeaad431434567b977601516bae6305294e1e
jorgegrajalesu <jegosystem@gmail.com>
2023-08-23 09:55:38 -0500

    230823

---
+++ js/variables.js
@@ -0,0 +1,15 @@
+/**
+ * los tipos de datos: string, number, boolean, date, object, 
+ * para declara variables: var, let, const (constante)
+ */
+
+// datos o tipos primitivos
+
+const miNombre =  'Jorge';
+const miNombre2 =  'Enrique';
+
+// miNombre = 'Enrique';
+
+let miEdad = 46;
+
+console.log(miNombre, miNombre2, miEdad);
