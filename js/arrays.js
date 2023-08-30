8fce95b207f3a9bd6a07e474ef84809d47c2690b
jorgegrajalesu <jegosystem@gmail.com>
2023-08-30 08:50:02 -0500

    30823

---
+++ js/arrays.js
@@ -0,0 +1,15 @@
+// sintaxis del array 
+
+let marcasAuto = [];//estaria declarando un array vacío
+
+const marcasAuto2 = [
+    30000,
+    'Mazda',
+    'Toyota',
+    'Ferrari',
+    'Mercedes'
+
+];
+
+// mostrar todas las marcas de los autos
+console.log(marcasAuto2);
