836e38ab48370b4ce51448e49a8835872441c653
jorgegrajalesu <jegosystem@gmail.com>
2023-09-06 08:42:23 -0500

    060923

--- js/arrays.js
+++ js/arrays.js
@@ -3,7 +3,7 @@
 let marcasAuto = [];//estaria declarando un array vacío
 
 const marcasAuto2 = [
-    30000,
+    'Renault',
     'Mazda',
     'Toyota',
     'Ferrari',
@@ -13,3 +13,24 @@ const marcasAuto2 = [
 
 // mostrar todas las marcas de los autos
 console.log(marcasAuto2);
+
+// mostrar la primera de marca de autos
+console.log('la primera marca de autos: ' , marcasAuto2[0]);
+
+// contar los elementos del array
+console.log('Cuántos son los elementos del array: ' , marcasAuto2.length);
+
+// mostrar el último elemento del array
+let ultimaMarca = marcasAuto2[marcasAuto2.length - 1];
+console.log('La última marca es: ' , ultimaMarca);
+
+// con el metodo PUSH para agregar un elemento al array
+let nuevaMarca = marcasAuto2.push('Ford');
+console.log(marcasAuto2 , nuevaMarca);
+
+// implementar al principio del array la marca BMW
+
+let marcaPrincpio = marcasAuto2.unshift('BMW');
+console.log(marcaPrincpio , marcasAuto2 );
+
+
